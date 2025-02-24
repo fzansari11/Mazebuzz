9c373845a09eabaea9ea439d8bb663a3317c1e60
fzansari11 <fahimzansari11@gmail.com>
2025-02-24 15:50:43 -0500

    Update common.py

--- src/maze/maze/common.py
+++ src/maze/maze/common.py
@@ -59,12 +59,6 @@ auth = HTTPBasicAuth()
 
 @auth.verify_password
 def verify_password(username_or_token, password):
-    # first try to authenticate by token
-    #if username_or_token in MAZE_ADMINS:
-    #    if password == 'BismillahMaze7865':
-    #        return True
-    #    else:
-    #        return False
     user = User.verify_auth_token(username_or_token)
     if not user:
         # try to authenticate with username/password
